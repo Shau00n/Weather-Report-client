24eab4a88d5a9a7de8a1a8e6da713cbeb14a94e3
Shau00n <flankeeeer7@gmail.com>
2022-12-21 21:36:16 +0900

    フォームの整理と追記

--- weather-report-client/src/pages/body/Body.js
+++ weather-report-client/src/pages/body/Body.js
@@ -1,16 +1,123 @@
-import { Link } from "react-router-dom";
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Navbar from '../../components/Navbar';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+// import Main from '../../layouts/Main';
 
 function Home() {
+    // useStateで管理している値
+    const [val, setVal] = React.useState('');
+
+    // テキストフィールドの値がチェンジされた時
+    const handleChange = (e) => {
+        setVal(e.target.value);
+    };
+
+    // バリデーション回り
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
     return (
-        <Form>
-            <H1>コンディションにあった服を提案してもらいますか？</H1>
-            <Button variant="primary" type="submit">
-                開始！
-            </Button>
-        </Form>
+        < >
+            <Navbar />
+            <DIV>
+                {/* 性別選択 */}
+                <Form.Select aria-label="Default select example">
+                    <option>性別を選択してください</option>
+                    <option value="1">男性</option>
+                    <option value="2">女性</option>
+                </Form.Select>
+                {/* 身長選択 */}
+                {/* 体重選択 */}
+                {/* 年齢選択 */}
+                {/* 活動レベル選択 */}
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="4" controlId="validationCustom01">
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="First name"
+                                defaultValue="Mark"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4" controlId="validationCustom02">
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Last name"
+                                defaultValue="Otto"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                            <Form.Label>Username</Form.Label>
+                            <InputGroup hasValidation>
+                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Username"
+                                    aria-describedby="inputGroupPrepend"
+                                    required
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Please choose a username.
+                                </Form.Control.Feedback>
+                            </InputGroup>
+                        </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="6" controlId="validationCustom03">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" placeholder="City" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid city.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom04">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control type="text" placeholder="State" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid state.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group as={Col} md="3" controlId="validationCustom05">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control type="text" placeholder="Zip" required />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid zip.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Form.Group className="mb-3">
+                        <Form.Check
+                            required
+                            label="Agree to terms and conditions"
+                            feedback="You must agree before submitting."
+                            feedbackType="invalid"
+                        />
+                    </Form.Group>
+                    <Button type="submit">Submit form</Button>
+                </Form>
+
+            </DIV>
+        </>
     );
 }
 
@@ -22,4 +129,21 @@ const H1 = styled.h1`
     font-size:  ${props => props.theme.x};
     font-weight: ${props => props.theme.bold};
     margin-bottom: 50px;
+`
+
+const H2 = styled.h2`
+    text-align: center;
+    font-size:  25px;
+    margin-bottom: 40px;
+    margin-top: 70px;
+
+`
+const DIV = styled.div`
+    text-align: center;
+    background-color: #EEFAFF;
+    margin-top: 30px;
+    margin-right: 100px;
+    margin-left: 100px;
+    padding: 80px
+
 `
