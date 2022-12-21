43e2a4d96d19c2b339ed87de3054da3943626a40
Shau00n <flankeeeer7@gmail.com>
2022-12-21 21:10:15 +0900

    ナビゲーションバーの追加

--- weather-report-client/src/pages/home/Home.js
+++ weather-report-client/src/pages/home/Home.js
@@ -1,26 +1,30 @@
-import { Link } from "react-router-dom";
 import styled from 'styled-components'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Navbar from '../../components/Navbar';
+// import Main from '../../layouts/Main';
 
 function Home() {
     return (
-        <DIV>
-            <Form>
-                <H1>コンディションにあった服を提案してもらいますか？</H1>
-                <Button variant="primary" type="submit" href="body" size="lg">
-                    測定開始！
-                </Button>
-                <H2>会員登録をして楽にコーディネイトを決めませんか？</H2>
-                <Button variant="primary" size="lg" active href="login">
-                    ログイン
-                </Button>{' '}
-                <Button variant="secondary" size="lg" active href="signup">
-                    新規会員登録
-                </Button>
-            </Form>
+        < >
+            <Navbar />
+            <DIV>
+                <Form>
+                    <H1>コンディションにあったコーディネイト<br />を提案してもらいますか？</H1>
+                    <Button variant="primary" type="submit" href="body" size="lg">
+                        測定開始！
+                    </Button>
+                    <H2>会員登録をして楽にコーディネイトを決めませんか？</H2>
+                    <Button variant="primary" size="lg" active href="login">
+                        ログイン
+                    </Button>{' '}
+                    <Button variant="secondary" size="lg" active href="signup">
+                        新規会員登録
+                    </Button>
+                </Form>
 
-        </DIV>
+            </DIV>
+        </>
     );
 }
 
@@ -37,14 +41,16 @@ const H1 = styled.h1`
 const H2 = styled.h2`
     text-align: center;
     font-size:  25px;
-    margin-bottom: 50px;
-    margin-top: 50px;
+    margin-bottom: 40px;
+    margin-top: 70px;
 
 `
 const DIV = styled.div`
     text-align: center;
     background-color: #EEFAFF;
-    margin: 80px;
+    margin-top: 30px;
+    margin-right: 100px;
+    margin-left: 100px;
     padding: 80px
 
 `
