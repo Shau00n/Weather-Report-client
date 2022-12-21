5c6f624d9d1d77472c9890172b4de760381c5052
Shau00n <flankeeeer7@gmail.com>
2022-12-21 21:09:43 +0900

    メインレイアウトを作成

---
+++ weather-report-client/src/layouts/Main.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import Navbar from '../components/Navbar';
+
+function Main() {
+    return (
+        < >
+            <Navbar />
+            <DIV>
+
+            </DIV>
+        </>
+    );
+}
+
+export default Main;
+
+
+const DIV = styled.div`
+    text-align: center;
+    background-color: #EEFAFF;
+    margin-top: 30px;
+    margin-right: 100px;
+    margin-left: 100px;
+    padding: 80px
+
+`
