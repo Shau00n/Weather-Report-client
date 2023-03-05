032ab847d731c52cbab0cb5b3acf0350381628e5
Shau00n <flankeeeer7@gmail.com>
2023-03-05 22:55:58 +0900

    form内容を変更（デザイン）

--- weather-report-client/src/pages/body/Body.js
+++ weather-report-client/src/pages/body/Body.js
@@ -1,120 +1,62 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Navbar from '../../components/Navbar';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
 // import Main from '../../layouts/Main';
 
 function Home() {
     // useStateで管理している値
-    const [val, setVal] = React.useState('');
-
-    // テキストフィールドの値がチェンジされた時
-    const handleChange = (e) => {
-        setVal(e.target.value);
-    };
-
-    // バリデーション回り
-    const [validated, setValidated] = useState(false);
-
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-        setValidated(true);
-    };
+    
 
     return (
         < >
             <Navbar />
             <DIV>
                 {/* 性別選択 */}
-                <Form.Select aria-label="Default select example">
+                <Form.Select aria-label="Default select example" class="form">
                     <option>性別を選択してください</option>
                     <option value="1">男性</option>
                     <option value="2">女性</option>
-                </Form.Select>
+                </Form.Select><br />
                 {/* 身長選択 */}
+                <Form.Label>身長を入力して下さい</Form.Label>
+                <Form.Control
+                    required
+                    type="number"
+                    placeholder="身長"
+                /><br />
                 {/* 体重選択 */}
+                <Form.Label>体重を入力して下さい</Form.Label>
+                <Form.Control
+                    required
+                    type="number"
+                    placeholder="体重"
+                    id='form-weight'
+                /><br />
                 {/* 年齢選択 */}
+                <Form.Label>年齢を入力して下さい</Form.Label>
+                <Form.Control
+                    required
+                    type="number"
+                    placeholder="年齢"
+                /><br />
                 {/* 活動レベル選択 */}
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} md="4" controlId="validationCustom01">
-                            <Form.Label>First name</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="First name"
-                                defaultValue="Mark"
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group as={Col} md="4" controlId="validationCustom02">
-                            <Form.Label>Last name</Form.Label>
-                            <Form.Control
-                                required
-                                type="text"
-                                placeholder="Last name"
-                                defaultValue="Otto"
-                            />
-                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                            <Form.Label>Username</Form.Label>
-                            <InputGroup hasValidation>
-                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Username"
-                                    aria-describedby="inputGroupPrepend"
-                                    required
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    Please choose a username.
-                                </Form.Control.Feedback>
-                            </InputGroup>
-                        </Form.Group>
-                    </Row>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} md="6" controlId="validationCustom03">
-                            <Form.Label>City</Form.Label>
-                            <Form.Control type="text" placeholder="City" required />
-                            <Form.Control.Feedback type="invalid">
-                                Please provide a valid city.
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group as={Col} md="3" controlId="validationCustom04">
-                            <Form.Label>State</Form.Label>
-                            <Form.Control type="text" placeholder="State" required />
-                            <Form.Control.Feedback type="invalid">
-                                Please provide a valid state.
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group as={Col} md="3" controlId="validationCustom05">
-                            <Form.Label>Zip</Form.Label>
-                            <Form.Control type="text" placeholder="Zip" required />
-                            <Form.Control.Feedback type="invalid">
-                                Please provide a valid zip.
-                            </Form.Control.Feedback>
-                        </Form.Group>
-                    </Row>
-                    <Form.Group className="mb-3">
-                        <Form.Check
-                            required
-                            label="Agree to terms and conditions"
-                            feedback="You must agree before submitting."
-                            feedbackType="invalid"
-                        />
-                    </Form.Group>
-                    <Button type="submit">Submit form</Button>
-                </Form>
+                <Form.Select aria-label="Default select example" class="form">
+                    <option>活動レベルを選択してください</option>
+                    <option value="1..0">椅子に座って静かにしている</option>
+                    <option value="1.4">立って仕事をしている</option>
+                    <option value="1.9">軽い荷物を運んでいる</option>
+                </Form.Select><br />
+
+                <DIV2 >
+                    <Button variant="secondary" size="lg" active href="/">
+                        前へ
+                    </Button>
+                    <Button variant="primary" size="lg" active href="result">
+                        次へ
+                    </Button>{' '}
+                </DIV2>
 
             </DIV>
         </>
@@ -147,3 +89,14 @@ const DIV = styled.div`
     padding: 80px
 
 `
+const DIV2 = styled.div`
+text-align: center;
+margin-top: 30px;
+
+`
+
+// .Select.Form `
+// weight: 80%;
+
+// `
+
